f67f8e24676b74e3a1592f81bf9b1c4229ea1a31
David McLennan <dmclennan92@gmail.com>
2019-01-17 23:52:04 +0000

    Add android lowercase button effect for chips

---
+++ src/OnlineShoppingTracker/OnlineShoppingTracker.Android/Effects/LowercaseButtonEffect.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using OnlineShoppingTracker.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("OnlineShoppingTracker")]
+[assembly: ExportEffect(typeof(LowercaseButtonEffect), "LowercaseButtonEffect")]
+namespace OnlineShoppingTracker.Droid.Effects
+{
+    public class LowercaseButtonEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var button = Control as AppCompatButton;
+
+                if (button != null)
+                {
+                    button.SetAllCaps(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
