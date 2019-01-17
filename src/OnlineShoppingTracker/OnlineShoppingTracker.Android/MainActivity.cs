55e13de16d8a7b551f220fc5e6285a1c844dd12d
David McLennan <dmclennan92@gmail.com>
2019-01-17 23:20:37 +0000

    Add product popup layout and styling
    Set android orientation to portrait

--- src/OnlineShoppingTracker/OnlineShoppingTracker.Android/MainActivity.cs
+++ src/OnlineShoppingTracker/OnlineShoppingTracker.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Prism.Ioc;
 
 namespace OnlineShoppingTracker.Droid
 {
-    [Activity(Label = "OnlineShoppingTracker", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "OnlineShoppingTracker", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
