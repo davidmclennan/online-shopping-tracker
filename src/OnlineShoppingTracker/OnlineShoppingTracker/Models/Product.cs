2d07b5762401e39a8eb570bf49943ccff78be739
David McLennan <dmclennan92@gmail.com>
2019-01-20 01:07:23 +0000

    Add product model binding and chip selection
    Added product model. Added product model bindings in product popup. Implemented chip selection through data triggers.

---
+++ src/OnlineShoppingTracker/OnlineShoppingTracker/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShoppingTracker.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Priority { get; set; }
+        public string Stage { get; set; }
+    }
+}
--- src/OnlineShoppingTracker/OnlineShoppingTracker/ViewModels/ProductPopupPageViewModel.cs
+++ src/OnlineShoppingTracker/OnlineShoppingTracker/ViewModels/ProductPopupPageViewModel.cs
@@ -3,14 +3,43 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using OnlineShoppingTracker.Models;
 
 namespace OnlineShoppingTracker.ViewModels
 {
 	public class ProductPopupPageViewModel : BindableBase
 	{
+        private Product product;
+        public Product Product
+        {
+            get { return product; }
+            set { SetProperty(ref product, value); }
+        }
+
+        public string Priority => Product.Priority;
+        public string Stage => Product.Stage;
+
+        public DelegateCommand<string> PriorityCommand { get; }
+        public DelegateCommand<string> StageCommand { get; }
+
         public ProductPopupPageViewModel()
         {
+            Product = new Product { Priority = "Low", Stage = "Wish List" };
 
+            PriorityCommand = new DelegateCommand<string>(ExecutePriorityCommand);
+            StageCommand = new DelegateCommand<string>(ExecuteStageCommand);
         }
-	}
+
+        private void ExecutePriorityCommand(string priority)
+        {
+            Product.Priority = priority;
+            RaisePropertyChanged(nameof(Priority));
+        }
+
+        private void ExecuteStageCommand(string stage)
+        {
+            Product.Stage = stage;
+            RaisePropertyChanged(nameof(Stage));
+        }
+    }
 }
