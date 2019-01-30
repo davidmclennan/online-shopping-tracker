05ca7b102012c4f29737c4b9359e60ad5018e8b7
David McLennan <dmclennan92@gmail.com>
2019-01-30 23:33:45 +0000

    Add purchase and return list UI and data
    Hook up data from inherited product list view model base.

--- src/OnlineShoppingTracker/OnlineShoppingTracker/ViewModels/PurchaseListPageViewModel.cs
+++ src/OnlineShoppingTracker/OnlineShoppingTracker/ViewModels/PurchaseListPageViewModel.cs
@@ -1,16 +1,31 @@
-﻿using Prism.Commands;
+﻿using OnlineShoppingTracker.Models;
+using Prism.AppModel;
+using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace OnlineShoppingTracker.ViewModels
 {
-	public class PurchaseListPageViewModel : BindableBase
+	public class PurchaseListPageViewModel : ProductListViewModelBase
 	{
-        public PurchaseListPageViewModel()
+        public PurchaseListPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+            : base(navigationService, dialogService)
         {
 
         }
-	}
+
+        public override async Task LoadProducts()
+        {
+            IsBusy = true;
+            Products = new ObservableCollection<Product>(await App.Database.GetProductsPurchasesAsync());
+            IsBusy = false;
+        }
+    }
 }
--- src/OnlineShoppingTracker/OnlineShoppingTracker/ViewModels/ReturnListPageViewModel.cs
+++ src/OnlineShoppingTracker/OnlineShoppingTracker/ViewModels/ReturnListPageViewModel.cs
@@ -1,16 +1,31 @@
-﻿using Prism.Commands;
+﻿using OnlineShoppingTracker.Models;
+using Prism.AppModel;
+using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace OnlineShoppingTracker.ViewModels
 {
-	public class ReturnListPageViewModel : BindableBase
+	public class ReturnListPageViewModel : ProductListViewModelBase
 	{
-        public ReturnListPageViewModel()
+        public ReturnListPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+            : base(navigationService, dialogService)
         {
 
         }
-	}
+
+        public override async Task LoadProducts()
+        {
+            IsBusy = true;
+            Products = new ObservableCollection<Product>(await App.Database.GetProductsReturnsAsync());
+            IsBusy = false;
+        }
+    }
 }
